850d0daf0ef26bf08c58f3c5671a58c06b81b668
Aruden <84431560+ArudenKun@users.noreply.github.com>
2023-05-15 20:30:21 +0800

    fix methods

--- pages/api/schedule/remove.ts
+++ pages/api/schedule/remove.ts
@@ -6,7 +6,7 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (req.method !== "POST") {
+	if (req.method !== "DELETE") {
 		res.status(405).json({ error: "Method not allowed" });
 	}
 
--- pages/api/schedule/update.ts
+++ pages/api/schedule/update.ts
@@ -6,7 +6,7 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (req.method !== "POST") {
+	if (req.method !== "PUT") {
 		res.status(405).json({ error: "Method not allowed" });
 	}
 
