e4983d72025fcda31aa4be4b3c291b2b1c1be71e
vjastoni <vjastoni13@gmail.com>
2023-05-29 10:39:13 +0800

    fix designs

--- components/calendar/stickynote.jsx
+++ components/calendar/stickynote.jsx
@@ -9,14 +9,14 @@ const StickyNote = (props) => {
     <div className={styles.schedule_container} onClick={props.onClick}>
       <div className={styles.schedule}>
         <p className={styles.course}>{props.course}</p>
-        <p className={styles.instructor}>{props.instructor}</p>
-        <p>{props.subject}</p>
-        <p className={styles.room}>{props.room}</p>
         <div className={styles.timeContainer}>
           <p className={styles.startTime}>{props.startTime}</p>
           {'-'}
           <p className={styles.endTime}>{props.endTime}</p>
         </div>
+        <p className={styles.instructor}>{props.instructor}</p>
+        <p>{props.subject}</p>
+        <p className={styles.room}>{props.room}</p>
       </div>
     </div>
   );
